cf6206dc1fb975d673b1172cf9aa2663e6e2a156
asanmankaket <624234014@parichat.skru.ac.th>
2022-06-30 02:40:47 +0700

    san30-06-65

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -12,17 +12,41 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+    int currentPageIndex = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(30),
-            child: Column(
-              children: [
-                Text('ข้อมูลของพนักงาน'),
+     bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'หน้าหลัก',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.how_to_reg),
+            label: 'ยืนยันการสมัคร',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.bookmark),
+            icon: Icon(Icons.person),
+            label: 'โปรไฟล์',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        Container(
+          // color: Colors.red,
+          alignment: Alignment.center,
+          child: Column(children: [
+             Text('ข้อมูลของพนักงาน'),
                 Text('จำนวนผู้สมัคร : XX'),
                 Text('จำนวนผู้ที่รอยืนยัน : XX'),
                 TextButton(
@@ -33,20 +57,27 @@ class _MainPageState extends State<MainPage> {
                               builder: (BuildContext context) =>
                                   const InputMentor()));
                     },
-                    child: Text('xxxxxxx'))
-              ],
-            ),
-          ),
-          TextButton(
-              onPressed: () {
-                Navigator.push(
+                    child: Text('xxxxxxx')),
+              TextButton(onPressed: () {
+                 Navigator.push(
                     context,
                     MaterialPageRoute<void>(
                         builder: (BuildContext context) => const AllMentor()));
-              },
-              child: Text('ดูพนักงานที่ทำงานได้'))
-        ],
-      ),
+              }, child: Text('ดูพนักงานที่ทำงานได้'))
+          ]),
+        ),
+        
+        Container(
+          // color: Colors.green,
+          alignment: Alignment.center,
+          child: const Text('Page 2'),
+        ),
+        Container(
+          // color: Colors.blue,
+          alignment: Alignment.center,
+          child: const Text('Page 3'),
+        ),
+      ][currentPageIndex],
       drawer: SideMenu(),
     );
   }
